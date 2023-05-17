8a64a91709b17354180c39e5b4fc61475ca10092
lichen39 <lichen39@58.com>
2023-05-17 22:54:49 +0800

    10

--- dist/assets/index-5c6706ae.js
+++
@@ -1,1 +0,0 @@
-import{o as e,k as t,a as s}from"./index-d8d4d952.js";const o={class:"search_bar"},c=s("input",{type:"text",placeholder:"请输入疾病名称进行搜索"},null,-1),_=[c],p={__name:"index",setup(a){return(n,r)=>(e(),t("div",o,_))}};export{p as _};
